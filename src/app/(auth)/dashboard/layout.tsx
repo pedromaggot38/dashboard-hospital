9c9c4bec1d76696ef0313f245384eff7ba92f936
Pedro Sanches <pedromaggot38@gmail.com>
2024-07-08 21:40:30 -0300

    adicionado react-quill e addpost

--- src/app/(auth)/dashboard/layout.tsx
+++ src/app/(auth)/dashboard/layout.tsx
@@ -1,9 +1,8 @@
 import Navbar from "@/app/(auth)/_ui/dashboard/navbar/navbar"
 import Sidebar from "@/app/(auth)/_ui/dashboard/sidebar/sidebar"
 import styles from "@/app/(auth)/dashboard/dashboard.module.css"
+import Footer from "@/app/(auth)/_ui/dashboard/footer/footer"
 import "./style.css"
-import Footer from "../_ui/dashboard/footer/footer"
-
 const Layout = ({ children }: { children: React.ReactNode }) => {
 
     return (
--- src/app/(auth)/dashboard/news/newpost/page.tsx
+++ src/app/(auth)/dashboard/news/newpost/page.tsx
@@ -1,18 +1,22 @@
 'use client'
 import { LuExternalLink, LuFileVideo, LuImagePlus, LuPlusCircle } from 'react-icons/lu'
 import styles from './newpost.module.css'
+import ReactQuill from 'react-quill'
+import "react-quill/dist/quill.bubble.css"
 import { useState } from 'react'
 const NewPost = () => {
 
     const [open, setOpen] = useState(false)
+    const [value, setValue] = useState("")
+
     return (
         <div className={styles.container}>
-            <input type="text" placeholder="Title" />
+            <input type="text" placeholder="Title" className={styles.input} />
             <div className={styles.editor}>
-                <button className={styles.button}>
+                <button className={styles.button} onClick={() => setOpen(!open)}>
                     <LuPlusCircle size={34} />
                 </button>
-                {open && <div className={styles.textOptions}>
+                {open && (<div className={styles.textOptions}>
                     <button>
                         <LuImagePlus className={styles.addButton} />
                     </button>
@@ -22,10 +26,13 @@ const NewPost = () => {
                     <button>
                         <LuFileVideo className={styles.addButton} />
                     </button>
-                </div>}
+                </div>
+                )}
+                <ReactQuill theme="bubble" value={value} onChange={setValue} placeholder="Content" className={styles.textArea} />
             </div>
+            <button className={styles.publishButton}>Publish</button>
         </div>
-    )
-}
+    );
+};
 
 export default NewPost
